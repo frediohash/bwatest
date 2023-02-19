3af0fca1eee3bbd85c7b4c47857a7540569dc63c
fredio.dimyati <fredio.dimyati@dwidasa.com>
2023-02-19 10:01:57 +0700

    loop data

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bwatest/users/user"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwatest?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	var users []user.User
+	db.Find(&users)
+	length := len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("=====================================")
+	}
+}
